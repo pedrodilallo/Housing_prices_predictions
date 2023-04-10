5269c72bccb381d9e2f8de4df119b7e0c50f5b58
pedrodilallo <113138619+pedrodilallo@users.noreply.github.com>
2023-04-10 20:27:28 -0300

    Correcting error with jupytext

---
+++ prices_analysis.py
@@ -0,0 +1,20 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.14.5
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+import pandas as pd
+
+print("qualquer_coisa")
+
+
