d1779159f1818dd48538b019bd52d0b110e88f91
Lucas Malheiros <lgdm1303@gmail.com>
2023-04-10 20:57:01 -0300

    Categorical data analysis

--- prices_analysis.py
+++ prices_analysis.py
@@ -15,15 +15,33 @@
 
 import pandas as pd
 import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 train = pd.read_csv("data/train.csv")
 test = pd.read_csv("data/test.csv")
 
-display(train.sample(7))
-display(test.sample(7))
+display(train.sample(5))
+
+display(test.sample(5))
+
+# # Sales price distribution
+
+sns.histplot(data=train["SalePrice"]);
+train["SalePrice"].describe()
 
 train_num_values = train.select_dtypes(["int64","float64"])
 train_disc_values = train.select_dtypes(["object"])
 train_disc_values.sample(5)
+
+# # Categorical variables
+
+# Categorical columns
+
+train_disc_values.columns
+
+# There are 43 categorical variables
+
+len(train_disc_values.columns)
 
 
